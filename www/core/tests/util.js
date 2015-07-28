a8ac7ac5802a1cd0fb363729d0db9aa4f5d6101b
Dani Palou <dpalou@cvaconsulting.com>
2015-07-28 18:59:38 +0200

    MOBILE-930 $mmUtil: Finish $mmUtil tests

--- www/core/tests/util.js
+++ www/core/tests/util.js
@@ -13,14 +13,40 @@
 // limitations under the License.
 
 describe('$mmUtil', function() {
-    var $mmUtil, $timeout, $httpBackend;
+    var $mmUtil,
+        $timeout,
+        $httpBackend,
+        $translate,
+        translationMockEN = {
+            'mm.core.day': 'day',
+            'mm.core.days': 'days',
+            'mm.core.hour': 'hour',
+            'mm.core.hours': 'hours',
+            'mm.core.min': 'min',
+            'mm.core.mins': 'mins',
+            'mm.core.sec': 'sec',
+            'mm.core.secs': 'secs',
+            'mm.core.year': 'year',
+            'mm.core.years': 'years',
+            'mm.core.now': 'now',
+            'mm.core.country-US': 'United States'
+        }, translationMockES = {
+            'mm.core.country-US': 'Estados Unidos'
+        };
 
     beforeEach(module('mm.core'));
 
-    beforeEach(inject(function(_$mmUtil_, _$timeout_, _$httpBackend_, $q) {
+    // Add mock translations.
+    beforeEach(module('pascalprecht.translate', function($translateProvider) {
+        $translateProvider.translations('en', translationMockEN).preferredLanguage('en');
+        $translateProvider.translations('es', translationMockES);
+    }));
+
+    beforeEach(inject(function(_$mmUtil_, _$timeout_, _$httpBackend_, _$translate_) {
         $mmUtil = _$mmUtil_;
         $timeout = _$timeout_;
         $httpBackend = _$httpBackend_;
+        $translate = _$translate_;
 
         $httpBackend.whenGET(/core\/assets\/mimetypes\.json/).respond(200, {
             "doc"  : {"type":"application/msword", "icon":"document", "groups":["document"]},
@@ -157,6 +183,232 @@ describe('$mmUtil', function() {
             }).finally(done);
 
             $httpBackend.flush();
+            $timeout.flush();
+        });
+
+    });
+
+    describe('getCountryName', function() {
+
+        it('should return country name translated', function() {
+            console.log(' ***** START $mmUtil getCountryName - translate ***** ');
+            var code = 'US',
+                stringKey = 'mm.core.country-' + code;
+            $translate.use('es');
+            expect($mmUtil.getCountryName(code)).toEqual(translationMockES[stringKey]);
+            $translate.use('en');
+            expect($mmUtil.getCountryName(code)).toEqual(translationMockEN[stringKey]);
+            console.log(' ***** FINISH $mmUtil getCountryName - translate ***** ');
+        });
+
+        it('should return country code if country not found', function() {
+            console.log(' ***** START $mmUtil getCountryName - not found ***** ');
+            var code = 'FAKE';
+            expect($mmUtil.getCountryName(code)).toEqual(code);
+            console.log(' ***** FINISH $mmUtil getCountryName - not found ***** ');
+        });
+
+    });
+
+    describe('getDocsUrl', function() {
+        var $mmLang,
+            $q,
+            lang = 'es';
+
+        beforeEach(inject(function(_$mmLang_, _$q_) {
+            $mmLang = _$mmLang_;
+            $q = _$q_;
+            // Mock $mmLang to quickly return the language. We use a language different than default.
+            $mmLang.getCurrentLanguage = function() {
+                return $q.when(lang);
+            };
+        }));
+
+        it('should return english URL if current language can\'t be retrieved', function(done) {
+            var page = 'page.html',
+                url = 'https://docs.moodle.org/en/' + page,
+                promise,
+                getCurrentLanguage = $mmLang.getCurrentLanguage;
+
+            // Make $mmLang.getCurrentLanguage to fail.
+            $mmLang.getCurrentLanguage = function() {
+                return $q.reject();
+            };
+
+            promise = $mmUtil.getDocsUrl(undefined, page);
+            promise.then(function(result) {
+                expect(result).toEqual(url);
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+
+            // Leave $mmLang as it was before.
+            $mmLang.getCurrentLanguage = getCurrentLanguage;
+        });
+
+        it('should return current language URL if it has been defined', function(done) {
+            var page = 'page.html',
+                url = 'https://docs.moodle.org/' + lang + '/' + page,
+                promise;
+
+            promise = $mmUtil.getDocsUrl(undefined, page);
+            promise.then(function(result) {
+                expect(result).toEqual(url);
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should return URL with release if it has been defined', function(done) {
+            var page = 'page.html',
+                release = '3.0dev (Build: 20150625)',
+                url = 'https://docs.moodle.org/30/' + lang + '/' + page,
+                promise;
+
+            promise = $mmUtil.getDocsUrl(release, page);
+            promise.then(function(result) {
+                expect(result).toEqual(url);
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+    });
+
+    describe('timestamp', function() {
+
+        it('should return the current timestamp', function() {
+            var timestamp = Math.round(new Date().getTime() / 1000),
+                result = $mmUtil.timestamp(),
+                difference = result - timestamp;
+            expect(difference).toBeLessThan(2); // We leave 1 second margin error because of Math.round.
+        });
+
+    });
+
+    describe('isFalseOrZero', function() {
+
+        it('should be able to identify if it is false or zero', function() {
+            expect($mmUtil.isFalseOrZero(0)).toEqual(true);
+            expect($mmUtil.isFalseOrZero('0')).toEqual(true);
+            expect($mmUtil.isFalseOrZero(false)).toEqual(true);
+            expect($mmUtil.isFalseOrZero()).toEqual(false);
+            expect($mmUtil.isFalseOrZero('false')).toEqual(false);
+            expect($mmUtil.isFalseOrZero(true)).toEqual(false);
+            expect($mmUtil.isFalseOrZero(1)).toEqual(false);
+            expect($mmUtil.isFalseOrZero(-1)).toEqual(false);
+        });
+
+    });
+
+    describe('isTrueOrOne', function() {
+
+        it('should be able to identify if it is true or one', function() {
+            expect($mmUtil.isTrueOrOne(1)).toEqual(true);
+            expect($mmUtil.isTrueOrOne('1')).toEqual(true);
+            expect($mmUtil.isTrueOrOne(true)).toEqual(true);
+            expect($mmUtil.isTrueOrOne()).toEqual(false);
+            expect($mmUtil.isTrueOrOne('true')).toEqual(false);
+            expect($mmUtil.isTrueOrOne(false)).toEqual(false);
+            expect($mmUtil.isTrueOrOne(0)).toEqual(false);
+            expect($mmUtil.isTrueOrOne(2)).toEqual(false);
+        });
+
+    });
+
+    describe('formatTime', function() {
+
+        it('should return now if 0 seconds or not specified', function(done) {
+            $mmUtil.formatTime().then(function(result) {
+                expect(result).toEqual('now');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $mmUtil.formatTime(0).then(function(result) {
+                expect(result).toEqual('now');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should convert 1 second in singular', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(1);
+            promise.then(function(result) {
+                expect(result).toEqual('1 sec');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should convert several seconds in plural', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(10);
+            promise.then(function(result) {
+                expect(result).toEqual('10 secs');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should return minutes and seconds if more than 60 seconds', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(100);
+            promise.then(function(result) {
+                expect(result).toEqual('1 min 40 secs');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should return days and hours if more than 24 hours', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(100001);
+            promise.then(function(result) {
+                expect(result).toEqual('1 day 3 hours');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should return years and days if more than 365 days', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(100000001);
+            promise.then(function(result) {
+                expect(result).toEqual('3 years 62 days');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
+            $timeout.flush();
+        });
+
+        it('should handle negative numbers as if they were positive', function(done) {
+            var promise;
+            promise = $mmUtil.formatTime(-100001);
+            promise.then(function(result) {
+                expect(result).toEqual('1 day 3 hours');
+            }).catch(function() {
+                expect(false).toBe(true);
+            }).finally(done);
+
             $timeout.flush();
         });
 
